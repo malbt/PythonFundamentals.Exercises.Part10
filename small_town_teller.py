e1870883f9f6b45628faa63f3f6b3e19ddb0f5e3
Maleda Tessema <mtessema@zipcodes-MacBook-Pro-3.local>
2020-03-12 22:41:55 -0400

    m

---
+++ small_town_teller.py
@@ -0,0 +1,17 @@
+class Person(object):
+    pass
+
+
+Person.id = ''
+Person.first_name = ''
+Person.last_name = ''
+
+
+class Account(object):
+    pass
+
+
+Account.number = ''
+Account.type = ''
+Account.owner = ''
+Account.balance = ''
