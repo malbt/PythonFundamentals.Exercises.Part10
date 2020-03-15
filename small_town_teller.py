1f0fffcc38b9089367d5da5f40be182cebffd679
Maleda Tessema <mtessema@zipcodes-MBP-3.lan>
2020-03-15 02:35:03 -0400

    almost

--- small_town_teller.py
+++ small_town_teller.py
@@ -1,50 +1,80 @@
+import types
+
+
 class Person:
-    def __init__(self, per_id, first_name, last_name):
-        Person.per_id = ''  # PK
-        Person.first_name = ''
-        Person.last_name = ''
+    def __init__(self, customer_id, first_name, last_name):
+        self.customer_id = customer_id
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.customer_id}, {self.first_name}, {self.last_name}'
+
+    def __repr__(self):
+        return f'{self.customer_id}, {self.first_name}, {self.last_name}'
 
 
 class Account:
-    def __init__(self, acc_number, acc_type, acc_owner, acc_balance):
-        Account.acc_number = ''  # PK
-        Account.acc_type = ''
-        Account.owner = ''  # not null or already in person
-        Account.acc_balance = ''
+    def __init__(self, acc_number, acc_type, acc_owner, balance):
+        self.acc_number = acc_number
+        self.acc_type = acc_type
+        self.acc_owner = acc_owner
+        self.balance = balance
 
-
-# Declare a Bank class able to support the following operations:
-#
-# Adding a customer to the bank
-# Adding an account to the bank
-# Removing an account from the bank
-# Depositing money into an account
-# Withdrawing money from an account
-# Balance inquiry for a particular account
-
-
-def withdraw():
-    amount = float(input("Enter amount to Withdraw: "))
+    def __repr__(self):
+        return f'{self.acc_number}, {self.acc_type}, {self.acc_owner}, {self.balance}'
 
 
 class Bank:
-    def deposit(self):
-        amount = float(input("Enter amount to be deposited: "))
-        self.balance += amount
-        print("\n Amount Deposited:", amount)
+    def __init__(self):
+        self.customer = []
+        self.account = {}
 
-    def deposit(self):
-        amount = float(input("Enter deposit: "))
-
-        self.balance += amount
-
-        print("Amount Deposited:", amount)
-
-        if self.balance >= amount:
-            self.balance -= amount
-            print("Withdrawal: ", amount)
+    def add_customer(self, person):
+        if person.customer_id in self.customer:
+            self.customer.append(person.customer_id)
         else:
-            print("Insufficient balance  ")
+            print("please register customer first!")
 
-    def balance_inquiry(self):
-        print("Available Balance : ", self.balance)
+    def add_account(self, account):
+        if account not in self.account:
+            self.account[account.acc_number] = account.balance
+        else:
+            print("Account number already exists!")
+
+    def deposit(self, acc_number, amount):
+        self.account[acc_balance] += amount
+
+    def withdraw(self, act_number, amount):
+        if self.account[acc_number] >= amount:
+            self.account[acc_bnumber] -= amount
+        else:
+            print("Transaction declined! ")
+
+    def balance_inquiry(self, acc_number):
+        print(f'Account Balance = {sel.account[account_number]}')
+
+
+from small_town_teller import Person, Account, Bank
+
+zc_bank = Bank()
+bob = Person(1, "Bob", "Smith")
+zc_bank.add_customer(bob)
+bob_savings = Account(1001, "SAVINGS", bob, balance)
+zc_bank.add_account(bob_savings)
+zc_bank.balance_inquiry(1001)
+# 0
+zc_bank.deposit(1001, 256.02)
+zc_bank.balance_inquiry(1001)
+# 256.02
+zc_bank.withdrawal(1001, 128)
+zc_bank.balance_inquiry(1001)
+# 128.02
+
+
+# Traceback (most recent call last):
+#   File "/Users/mtessema/PythonFundamentals.Exercises.Part10/small_town_teller.py", line 58, in <module>
+#     from small_town_teller import Person, Account, Bank
+#   File "/Users/mtessema/PythonFundamentals.Exercises.Part10/small_town_teller.py", line 63, in <module>
+#     bob_savings = Account(1001, "SAVINGS", bob, balance)
+# NameError: name 'balance' is not defin
